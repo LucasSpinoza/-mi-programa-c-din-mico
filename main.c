d5e32d2d726346252b96e021732915e864839b50
Lucas Spinoza <lucas.spinoza@alumnos.frm.utn.edu.ar>
2026-09-18 21:38:41 -0300

    refactor: Mensaje final de programa y liberación de memoria

--- main.c
+++ main.c
@@ -30,7 +30,6 @@ int main() {
 	// Liberar la memoria asignada
 	// Es muy importante liberar la memoria dinámica cuando ya no se necesita para evitar fugas de memoria.
 	free(arr);
-	printf("Memoria liberada.\n");
-	
+	printf("Programa finalizado y memoria liberada.\n");
 	return 0; // Terminación exitosa del programa
 }
